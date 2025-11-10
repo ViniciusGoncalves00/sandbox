98494e006d18961118fb3c2f92905e959ec29388
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-11-09 22:56:38 -0300

    added gui

--- src/perlinnoise/main.ts
+++ src/perlinnoise/main.ts
@@ -3,6 +3,7 @@ import { WebGPURenderer } from "three/webgpu";
 import { MathUtils } from "@viniciusgoncalves/ts-utils";
 import { PerlinNoise2D } from "./perlin-noise-2D-classic";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const scene = new THREE.Scene();
@@ -15,9 +16,9 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 
-camera.position.x = -50;
+camera.position.x = -20;
 camera.position.y = 50;
-camera.position.z = -50;
+camera.position.z = -20;
 camera.lookAt(0, 0, 0);
 // camera.rotateY(MathUtils.deg2rad(90));
 
@@ -32,27 +33,65 @@ function onResizeWindow() {
 	renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 }
 
-const sizeX = 100;
-const sizeY = 100;
 const perlinNoise2D = new PerlinNoise2D(scene);
+perlinNoise2D.generate();
 
-
-let lastMeshUpdate = 0;
-const meshUpdateInterval = 200;
-
-function animate(time: number) {
+function animate() {
     requestAnimationFrame(animate);
 
     renderer.renderAsync(scene, camera);
-
-    if (time - lastMeshUpdate >= meshUpdateInterval) {
-        lastMeshUpdate = time;
-
-        scene.clear();
-        perlinNoise2D.generateControlGrid(sizeX, sizeY);
-        perlinNoise2D.generateMesh(sizeX, sizeY);
-        perlinNoise2D.centralize();
-    }
 }
 
-animate(0);
+animate();
+
+const gui = new GUI();
+gui.title("Configuration");
+
+const parameters = gui.addFolder("Parameters");
+const debug = gui.addFolder("Debug");
+
+
+const grid = parameters.addFolder("Grid");
+const size = grid.addFolder("Size");
+size.add( perlinNoise2D, "gridSizeX", 1, 1000 ).name( "X").step(1).onChange(value => perlinNoise2D.gridSizeX = value);
+size.add( perlinNoise2D, "gridSizeZ", 1, 1000 ).name( "Z").step(1).onChange(value => perlinNoise2D.gridSizeZ = value);
+
+const controlNodes = parameters.addFolder("Control Nodes");
+const controlNodesCells = controlNodes.addFolder("Cells Amount");
+controlNodesCells.add( perlinNoise2D, "controlNodesCellsAmountX", 1, 100, 1).name( "X").onChange(value => perlinNoise2D.controlNodesCellsAmountX = value);
+controlNodesCells.add( perlinNoise2D, "controlNodesCellsAmountZ", 1, 100, 1).name( "Z").onChange(value => perlinNoise2D.controlNodesCellsAmountZ = value);
+controlNodesCells.add( perlinNoise2D, "randomizeDirection").name( "randomize directions").onChange(value => perlinNoise2D.randomizeDirection = value);
+
+const mesh = parameters.addFolder("Mesh");
+const meshCells = mesh.addFolder("Quads Amount");
+meshCells.add( perlinNoise2D, "meshCellsAmountX", 1, 1000, 1).name( "X").onChange(value => perlinNoise2D.meshCellsAmountX = value);
+meshCells.add( perlinNoise2D, "meshCellsAmountZ", 1, 1000, 1).name( "Z").onChange(value => perlinNoise2D.meshCellsAmountZ = value);
+
+const transformation = parameters.addFolder("Transformation");
+const scale = transformation.addFolder("Scale");
+// scale.add(perlinNoise2D.mesh.scale, "x", -1, 1, 0.1).onChange((value: number) => perlinNoise2D.mesh.scale.setX(value));
+scale.add(perlinNoise2D.mesh.scale, "y", -1, 1, 0.1).onChange((value: number) => perlinNoise2D.mesh?.scale.setY(value));
+// scale.add(perlinNoise2D.mesh.scale, "z", -1, 1, 0.1).onChange((value: number) => perlinNoise2D.mesh?.scale.setZ(value));
+
+// const controlNodeCellSizeX = gui.add( perlinNoise2D, "controlNodeCellSizeX", 0, 0 ).name( "Mesh Cell Size X").disable().updateDisplay();
+// const controlNodeCellSizeZ = gui.add( perlinNoise2D, "controlNodeCellSizeZ", 0, 0 ).name( "Mesh Cell Size Z").disable().updateDisplay();
+// const meshCellSizeX = gui.add( perlinNoise2D, "meshCellSizeX", 0, 0 ).name( "Mesh Cell Size Z").disable().updateDisplay();
+// const meshCellSizeZ = gui.add( perlinNoise2D, "meshCellSizeZ", 0, 0 ).name( "Mesh Cell Size Z").disable().updateDisplay();
+
+debug.add(perlinNoise2D, "showControlNodes").name("Show Control Nodes").onChange((value) => {
+    perlinNoise2D.showControlNodes = value;
+    perlinNoise2D.controlGridSpheres.visible = value;
+});
+// debug.add(perlinNoise2D, "controlNodeSize", 1, 2).name("Control Node Size").onChange((value) => {
+//     perlinNoise2D.controlNodeSize = value;
+//     perlinNoise2D.sphereGeometry.scale(value, value, value);
+// });
+debug.add(perlinNoise2D, "showDirectionArrow").name("Show Direction Arrows").onChange((value) => {
+    perlinNoise2D.showDirectionArrow = value;
+    perlinNoise2D.directionArrows.visible = value;
+});
+
+// gui.add( perlinNoise2D, "controlNodeCellSizeX", 0, 0 ).name( "Mesh Cell Size X").disable();
+// gui.add( perlinNoise2D, "controlNodeCellSizeX", 0, 0 ).name( "Mesh Cell Size X").disable();
+
+gui.add( perlinNoise2D, "generate").name( "Generate");
--- src/perlinnoise/perlin-noise-2D-classic.ts
+++ src/perlinnoise/perlin-noise-2D-classic.ts
@@ -1,79 +1,105 @@
-import { BufferAttribute, BufferGeometry, Color, DoubleSide, Material, Mesh, MeshBasicMaterial, Scene, SphereGeometry, Vector2, Vector3, type Vector2Like } from "three";
+import { ArrowHelper, BufferAttribute, BufferGeometry, Color, DoubleSide, Material, Mesh, MeshBasicMaterial, Scene, SphereGeometry, Vector2, Vector3, type Vector2Like } from "three";
 import { ControlNode } from "./node";
 import { Square } from "./square";
 import { lerp, seededRandom } from "three/src/math/MathUtils.js";
 
 export class PerlinNoise2D {
     public controlGrid: ControlNode<Vector3>[][] | null = null;
-    public controlGridSpheres: Mesh | null = null;
-    public mesh: Mesh | null = null;
+
+    // #region [Mesh]
+    public mesh: Mesh
+    public controlGridSpheres: Mesh;
+    public directionArrows: Mesh
+
+    public sphereGeometry: SphereGeometry;
+    public controlNodeDebugSphere: Mesh;
+    // #endregion
 
     // #region [parameters]
     public gridSizeX: number = 100;
     public gridSizeZ: number = 100;
 
-    public controlNodesCellsAmountX: number = 5;
-    public controlNodesCellsAmountZ: number = 5;
+    public controlNodesCellsAmountX: number = 1;
+    public controlNodesCellsAmountZ: number = 1;
+    public randomizeDirection: boolean = true;
+
     public meshCellsAmountX = 100;
     public meshCellsAmountZ = 100;
 
-    private controlNodeCellSizeX: number = 1;
-    private controlNodeCellSizeZ: number = 1;
-    private meshCellSizeX: number = 1;
-    private meshCellSizeZ: number = 1;
+    public controlNodeCellSizeX: number = 1;
+    public controlNodeCellSizeZ: number = 1;
+    public meshCellSizeX: number = 1;
+    public meshCellSizeZ: number = 1;
     // #endregion
 
     // #region [visualization]
-    private controlNodeDebugSphere: Mesh;
+    public showControlNodes: boolean = true;
+    public showDirectionArrow: boolean = true;
+    
+    // public controlNodeSize: number = 0.1;
+    // public directionArrowLength: number = 1;
+
     private scene: Scene;
     // #endregion
 
-    public constructor(scene: Scene) {
+    public constructor(scene: Scene, ) {
         this.scene = scene;
 
-        const geometry = new SphereGeometry(0.1);
+        this.sphereGeometry = new SphereGeometry(1);
+        this.sphereGeometry.scale(0.1, 0.1, 0.1);
         const material = new MeshBasicMaterial({ color: new Color("red")});
-        this.controlNodeDebugSphere = new Mesh(geometry, material);
+        this.controlNodeDebugSphere = new Mesh(this.sphereGeometry, material);
+
+        this.mesh = new Mesh();
+        this.scene.add(this.mesh);
+
+        this.controlGridSpheres = new Mesh();
+        this.scene.add(this.controlGridSpheres);
+
+        this.directionArrows = new Mesh();
+        this.scene.add(this.directionArrows);
     }
 
-    public generate(sizeX?: number, sizeY?: number): void {
-        this.generateControlGrid(sizeX, sizeY);
-        this.generateMesh(sizeX, sizeY);
+    public generate(): void {
+        if(this.randomizeDirection) {
+            this.controlGridSpheres.clear();
+            this.directionArrows.clear();
+            this.generateControlGrid();
+        }
+
+        this.mesh.clear();
+        this.generateMesh();
         this.centralize();
     }
 
-    public generateControlGrid(gridSizeX?: number, gridSizeZ?: number): void {
-        const sizeX = gridSizeX ? gridSizeX : this.gridSizeX;
-        const sizeZ = gridSizeZ ? gridSizeZ : this.gridSizeZ;
-
-        this.controlNodeCellSizeX = sizeX / this.controlNodesCellsAmountX;
-        this.controlNodeCellSizeZ = sizeZ / this.controlNodesCellsAmountZ;
+    private generateControlGrid(): void {
+        this.controlNodeCellSizeX = this.gridSizeX / this.controlNodesCellsAmountX;
+        this.controlNodeCellSizeZ = this.gridSizeZ / this.controlNodesCellsAmountZ;
 
         this.controlGrid = [];
-        this.controlGridSpheres = new Mesh();
 
         for (let x = 0; x <= this.controlNodesCellsAmountX; x++) {
             this.controlGrid[x] = [];
             for (let z = 0; z <= this.controlNodesCellsAmountZ; z++) {
                 const position = new Vector3(x * this.controlNodeCellSizeX, 0, z * this.controlNodeCellSizeZ);
-                const direction = new Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+                const direction = new Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1).normalize();
                 const mesh = this.controlNodeDebugSphere.clone();
                 mesh.position.set(position.x, position.y, position.z);
                 this.controlGridSpheres.add(mesh);
 
                 this.controlGrid[x][z] = new ControlNode(position, direction, this.controlNodeDebugSphere);
+
+                const length = 10;
+                const hex = 0xffff00;
+                const arrowHelper = new ArrowHelper( direction, position, length, hex );
+                this.directionArrows.add( arrowHelper );
             }
         }
-
-        this.scene.add(this.controlGridSpheres);
     }
 
-    public generateMesh(gridSizeX?: number, gridSizeZ?: number): void {
-        const sizeX = gridSizeX ? gridSizeX : this.gridSizeX;
-        const sizeZ = gridSizeZ ? gridSizeZ : this.gridSizeZ;
-
-        this.meshCellSizeX = sizeX / this.meshCellsAmountX;
-        this.meshCellSizeZ = sizeZ / this.meshCellsAmountZ;
+    private generateMesh(): void {
+        this.meshCellSizeX = this.gridSizeX / this.meshCellsAmountX;
+        this.meshCellSizeZ = this.gridSizeZ / this.meshCellsAmountZ;
 
         const vertices: Vector3[][] = [];
 
@@ -175,12 +201,11 @@ export class PerlinNoise2D {
         const buffer = new BufferGeometry().setAttribute("position", attribute);
         buffer.computeVertexNormals();
         const material = new MeshBasicMaterial({ color: new Color("green"), side: DoubleSide, wireframe: true});
-        this.mesh = new Mesh(buffer, material);
-
-        this.scene.add(this.mesh);
+        const mesh = new Mesh(buffer, material);
+        this.mesh.add(mesh);
     }
 
-    public getNeighbors(coordinates: Vector3): Square<ControlNode<Vector3>> | null {
+    private getNeighbors(coordinates: Vector3): Square<ControlNode<Vector3>> | null {
         if (!this.controlGrid) return null;
 
         const [x, z] = this.coordinates2Index(coordinates);
@@ -193,7 +218,7 @@ export class PerlinNoise2D {
         return new Square(botLeft, botRight, topRight, topLeft);
     }
 
-    public coordinates2Index(coordinates: Vector3): [number, number] {
+    private coordinates2Index(coordinates: Vector3): [number, number] {
         const nx = coordinates.x / this.controlNodeCellSizeX;
         const nz = coordinates.z / this.controlNodeCellSizeZ;
 
@@ -203,17 +228,16 @@ export class PerlinNoise2D {
         return [x, y];
     }
 
-    public getWeight(coordinates: Vector3, controlNode: ControlNode<Vector3>): number {
+    private getWeight(coordinates: Vector3, controlNode: ControlNode<Vector3>): number {
         const delta = new Vector3().subVectors(coordinates, controlNode.position);
         return controlNode.direction.dot(delta);
     }
 
-    public interpolate(t: number): number {
+    private interpolate(t: number): number {
         return t * t * t * (t * (t * 6 - 15) + 10);
     }
 
-    public centralize(): void {
-        if(!this.mesh || !this.controlGridSpheres) return;
+    private centralize(): void {
         this.mesh.geometry.computeBoundingBox();
 
         let center = new Vector3();
@@ -225,5 +249,9 @@ export class PerlinNoise2D {
         this.controlGridSpheres.translateX(-center.x);
         this.controlGridSpheres.translateY(-center.y);
         this.controlGridSpheres.translateZ(-center.z);
+
+        this.directionArrows.translateX(-center.x);
+        this.directionArrows.translateY(-center.y);
+        this.directionArrows.translateZ(-center.z);
     }
 }
