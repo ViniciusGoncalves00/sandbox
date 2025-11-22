525ea9d9fcae0fa2131b345a2a1a930e324ece89
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-11-22 20:15:13 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import CopyPlugin from "copy-webpack-plugin";
-import { fileURLToPath } from "url";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
-  mode: "production",
-
-  entry: "./src/main.ts",
+module.exports = {
+  entry: {
+    main: "./src/main.ts",
+    terrain: "./src/Terrain/terrain.ts",
+    handler: "./src/Handler/outra.ts",
+  },
 
   output: {
-    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
     clean: true,
-    publicPath: "",
   },
 
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/,
-      },
-    ],
+      }
+    ]
   },
 
   resolve: {
@@ -33,33 +29,22 @@ export default {
   },
 
   plugins: [
-    // Copia toda a pasta src/ para dist/ (exceto .ts)
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "src",
-          to: ".",
-          globOptions: {
-            ignore: ["**/*.ts", "**/*.tsx"], // ignorar arquivos TS
-          },
-        },
-      ],
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"],
     }),
 
-    // Injeta o bundle apenas no index.html principal
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/index.html"),
-        filename: "index.html",
-        inject: "body",
-    })
-  ],
+      template: "./src/Terrain/index.html",
+      filename: "Terrain/index.html",
+      chunks: ["terrain"],
+    }),
 
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "src"), // servir src como raiz no DEV
-    },
-    open: true,
-    hot: true,
-    port: 3000,
-  },
+    new HtmlWebpackPlugin({
+      template: "./src/Handler/index.html",
+      filename: "Handler/index.html",
+      chunks: ["handler"],
+    }),
+  ]
 };
