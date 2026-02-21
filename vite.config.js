21fd50e467ecb999e6014deb393a453e8e1d2f25
Vinicius <viniciusgoncalves2000@hotmail.com>
2026-02-20 23:39:25 -0300

    auto pathing

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from "vite";
 import path from "path";
+import fs from "fs";
+
+function getEntries() {
+  const base = path.resolve(__dirname, "src");
+  const entries = {};
+
+  for (const dir of fs.readdirSync(base)) {
+    const indexPath = path.join(base, dir, "index.html");
+    if (fs.existsSync(indexPath)) {
+      entries[dir] = indexPath;
+    }
+  }
+
+  return entries;
+}
 
 export default defineConfig({
   base: "/sandbox/",
-
   build: {
     rollupOptions: {
-      input: {
-        example: path.resolve(__dirname, "src/example/index.html"),
-        fluids: path.resolve(__dirname, "src/fluids/index.html"),
-        handler: path.resolve(__dirname, "src/handler/index.html"),
-        marchingsquares: path.resolve(__dirname, "src/marchingsquares/index.html"),
-        snake: path.resolve(__dirname, "src/snake/index.html"),
-        terrain: path.resolve(__dirname, "src/terrain/index.html"),
-      },
+      input: getEntries(),
     },
   },
 });
